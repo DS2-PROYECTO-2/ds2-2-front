3717ed881eac1f3866d4299c139e55bd78c1fb1f
HDGM <garcia.hernan@correounivalle.edu.co>
2025-09-30 19:18:25 -0500

    EISC-135 Frontend: Integración API y manejo de sesión revision #4

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,10 @@
-import { useAuth as useAuthContext } from '../context/AuthContext'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
-export const useAuth = useAuthContext
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
